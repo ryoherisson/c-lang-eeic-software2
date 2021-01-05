544da29a8ea3adc12b13503910a22a4518ad49b9
ryoherisson <ryhizw@gmail.com>
2021-01-06 00:02:24 +0900

    add week5

---
+++ week5/func_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int add_one(int x) {
+    return x + 1;
+}
+
+int main() {
+    int (*fp)(int);
+
+    fp = &add_one;
+    // fp = add_one; でも問題ない
+
+    int x = 1;
+
+    int y = (*fp)(x);
+
+    printf("%d\n", y);
+
+    return 0;
+}
