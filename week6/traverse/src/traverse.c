ff653e82f6e708b201d2bc3c549483454a265cd4
ryoherisson <ryhizw@gmail.com>
2021-01-08 10:14:26 +0900

    add traverse

---
+++ week6/traverse/src/traverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "traverse.h"
+
+void traverse(const Node *n)
+{
+  if (n == NULL) return;
+
+  // 以下に木を操作する手順を実装する
+  // どうすればよいか？
+  // 表示は printf("value = %d\n", n->value); のような形で良い
+
+  printf("value=%d\n", n->value);
+  traverse(n->left);
+  traverse(n->right);
+
+}
